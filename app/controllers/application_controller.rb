c17efb60f2b99b105d11b9b106912e6cb0442be6
Sara Beth Ribot <sbribot@gmail.com>
2018-06-28 23:16:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  
+  post '/' do 
+    @x = params["firstnumber"]
+    @y = params["secondnumber"]
+    @answer = multiply(@x, @y)
+    erb :results
 end
+
+end 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,6 @@
-class Model
-  # Replace with Dog clas
-end
+def multiply(x,y)
+  new_x = x.to_i 
+  new_y = y.to_i
+  answer = (new_x)*(new_y)
+end 
+
